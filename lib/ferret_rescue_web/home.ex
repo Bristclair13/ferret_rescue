ba9a61e989b6416a4a3973b4f08dfc27d70df07d
Brianna St Clair <briannastclair13@gmail.com>
2024-09-03 09:21:55 -0600

    working on home page

--- lib/ferret_rescue_web/home.ex
+++ lib/ferret_rescue_web/home.ex
@@ -3,7 +3,23 @@ defmodule FerretRescueWeb.Live.Home do
 
   def render(assigns) do
     ~H"""
-    <div class=" h-full bg-[url('/images/tree_bark.png')]  bg-repeat"></div>
+    <div class=" h-full bg-[url('/images/tree_bark.png')]  bg-repeat">
+      <div>
+        <p class="text-center pt-8 pb-8">Texas Ferret Lover's Rescue</p>
+        <p class="text-center">
+          Our Mission: We're saving lives, as many as we can. Finding forever homes for our theiving friends.
+        </p>
+        <button class="place-content-center">Adopt A Ferret!</button>
+      </div>
+      <div class="flex justify-between  max-w-6xl mx-auto h-32">
+        <img src="/images/ferret-1.jpg" />
+        <img src="/images/ferret-2.jpg" />
+        <img src="/images/ferret-3.jpg" />
+        <img src="/images/ferret-4.jpg" />
+        <img src="/images/ferret-5.jpg" />
+        <img src="/images/ferret-6.jpg" />
+      </div>
+    </div>
     """
   end
 end
