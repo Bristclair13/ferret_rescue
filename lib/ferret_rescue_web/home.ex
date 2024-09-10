0ccf3f6e7523b390d4b2458e0c7f3b3bedd8e7e6
Brianna St Clair <briannastclair13@gmail.com>
2024-09-10 17:28:57 -0600

    working on home page

--- lib/ferret_rescue_web/home.ex
+++ lib/ferret_rescue_web/home.ex
@@ -3,15 +3,23 @@ defmodule FerretRescueWeb.Live.Home do
 
   def render(assigns) do
     ~H"""
-    <div class=" h-full bg-[url('/images/tree_bark.png')]  bg-repeat">
-      <div>
-        <p class="text-center pt-8 pb-8">Texas Ferret Lover's Rescue</p>
-        <p class="text-center">
-          Our Mission: We're saving lives, as many as we can. Finding forever homes for our theiving friends.
-        </p>
-        <button class="place-content-center">Adopt A Ferret!</button>
+    <div class="h-full bg-[url('/images/tree_bark.png')]  bg-repeat">
+      <div class="flex items-center border-4 border-black bg-white max-w-6xl mx-auto">
+        <div>
+          <img src="/images/logo.jpg" class="h-32 pl-4" />
+        </div>
+        <div class="flex flex-col mx-auto">
+          <p class="text-center text-4xl pt-8 pb-8">Texas Ferret Lover's Rescue</p>
+          <p class="text-center">
+            Our Mission: We're saving lives, as many as we can. Finding forever homes for our theiving friends.
+          </p>
+          <button class="place-content-center">Adopt A Ferret!</button>
+        </div>
+        <div>
+          <img src="/images/logo.jpg" class="h-32 pr-4" />
+        </div>
       </div>
-      <div class="flex justify-between  max-w-6xl mx-auto h-32">
+      <div class="flex justify-between  max-w-6xl mx-auto h-32 box-shadow-md mt-6">
         <img src="/images/ferret-1.jpg" />
         <img src="/images/ferret-2.jpg" />
         <img src="/images/ferret-3.jpg" />
