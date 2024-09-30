fea7adbdb8bf0d0b53b6d3959620555916add32e
Brianna St Clair <briannastclair13@gmail.com>
2024-09-30 10:43:54 -0600

    got faq drop down button working

--- lib/ferret_rescue_web/live/faq.ex
+++ lib/ferret_rescue_web/live/faq.ex
@@ -7,7 +7,17 @@ defmodule FerretRescueWeb.Live.Faq do
       <h3 class="text-3xl">Frequently Asked Questions</h3>
       <div class="mt-2 p-4 shadow-lg bg-white flex flex-col">
         <div>
-          <h3 class="text-3xl">Basic</h3>
+          <h3 class="text-3xl mb-1">Basic</h3>
+          <button
+            type="button"
+            class="p-3 border rounded bg-gray-50"
+            phx-click={toggle("#ferrets-eat")}
+          >
+            What should ferrets eat?
+          </button>
+          <p id="ferrets-eat" class="hidden px-4 py-6 border">
+            Ferrets are obligate carnivores. They need high protein and fat, no carbs, so they must be fed a good quality ferret food. There are several on the market that are good including: Totally Ferret, Marshal Farms, and Natural Gold. Our rescue sells Totally Ferret Active, Totally Ferret Venison and Wysong. Do not feed ferrets anything that has sugar in it, including raisins, bananas, Yogi's, ect... A good treat would be Turkey or Chicken and Gravy baby food (Delmonte or Gerber's). Also, Foamy Fries are good too and Ferret N-Bone Chew Treat (great for their teeth).
+          </p>
         </div>
         <div class="mt-6">
           <h3 class="text-3xl">Adoption</h3>
