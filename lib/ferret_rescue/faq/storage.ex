cca83194686c37ea29d201d8678de9edbe3c1736
Brianna St Clair <briannastclair13@gmail.com>
2024-10-07 18:45:00 -0400

    attempting faq

--- lib/ferret_rescue/faq/storage.ex
+++ lib/ferret_rescue/faq/storage.ex
@@ -4,4 +4,14 @@ defmodule FerretRescue.Faq.Storage do
   alias FerretRescue.Schemas.Topic
   alias FerretRescue.Schemas.Content
   alias FerretRescue.Repo
+
+  @callback list_topics(String.t()) :: [Topic.t()]
+  def list_topics() do
+    from(Topic, order_by: :rank) |> Repo.all()
+  end
+
+  @callback list_content(String.t()) :: [Content.t()]
+  def list_content() do
+    from(Content, order_by: :rank) |> Repo.all()
+  end
 end
