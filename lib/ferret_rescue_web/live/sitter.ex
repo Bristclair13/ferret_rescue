1f3abc84c0d712722cc1db5b230021497ee7afa9
Brianna St Clair <briannastclair13@gmail.com>
2024-10-07 13:12:01 -0500

    attempting to get layout right

--- lib/ferret_rescue_web/live/sitter.ex
+++ lib/ferret_rescue_web/live/sitter.ex
@@ -15,78 +15,29 @@ defmodule FerretRescueWeb.Live.Sitter do
       <p class="text-center font-light mt-6 bg-white p-6 shadow-lg">
         We do not guarantee any of these sitters, you are fully responsible for ensuring the sitter fits your needs. By using this website to find a sitter you agree to not hold us responsible for the actions of these sitters.
       </p>
-      <div class="flex">
-        <div class="flex flex-col w-1/2 mt-6 mr-6 p-6 bg-white shadow-lg">
-          <div class="flex justify-between">
-            <p>Barbara Bullock</p>
-            <a href="tel:+19723107440" class="hover:underline text-blue-400 hover:text-blue-500">
-              (972) 310-7440
-            </a>
-          </div>
-          <div>
-            <a
-              href="mailto:txflrvp@gmail.com"
-              class="hover:underline text-blue-400 hover:text-blue-500"
-            >
-              txflrvp@gmail.com
-            </a>
-            <p class="font-bold mt-3">Info:</p>
-            <p class="font-light">
-              Located in Plano area. Has private, separate room for the ferrets. Experience with medically challenged ferrets and behavioral issues.
-            </p>
-          </div>
-        </div>
-        <div class="flex flex-col w-1/2 mt-6 p-6 bg-white shadow-lg">
-          <div class="flex justify-between">
-            <p>Jenn Solberg</p>
-            <a href="tel:+12143369984" class="hover:underline text-blue-400 hover:text-blue-500">
-              214-336-9984
-            </a>
-          </div>
-          <div>
-            <a
-              href="mailto:Jenn3246@att.net"
-              class="hover:underline text-blue-400 hover:text-blue-500"
-            >
-              Jenn3246@att.net
-            </a>
-            <p class="font-bold mt-3">Info:</p>
-            <p class="font-light">
-              Located in North Central Dallas. Serves DFW area. I have extensive experience with both Medical and behavior issues.
-            </p>
+      <div :for={sitter <- @sitters}>
+        <div class="grid grid-cols-2">
+          <div class="flex flex-col mt-6 mr-6 p-6 bg-white shadow-lg border-2 border-black">
+            <div class="flex justify-between">
+              <p><%= sitter.name() %></p>
+              <a href="tel:+19723107440" class="hover:underline text-blue-400 hover:text-blue-500">
+                <%= sitter.phone() %>
+              </a>
+            </div>
+            <div>
+              <a
+                href="mailto:txflrvp@gmail.com"
+                class="hover:underline text-blue-400 hover:text-blue-500"
+              >
+                <%= sitter.email() %>
+              </a>
+              <p class="font-bold mt-3">Info:</p>
+              <p class="font-light">
+                <%= sitter.notes() %>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="w-1/2 mt-6 p-6 bg-white shadow-lg">
-        <div class="flex justify-between">
-          <p>Kim Droptini</p>
-          <a href="tel:+12145144411" class="hover:underline text-blue-400 hover:text-blue-500">
-            (214) 514-4411
-          </a>
-        </div>
-        <div>
-          <a
-            href="mailto:Kdroptini@outlook.com"
-            class="hover:underline text-blue-400 hover:text-blue-500"
-          >
-            Kdroptini@outlook.com
-          </a>
-          <p class="font-bold mt-3">Info:</p>
-          <p class="font-light">
-            Tarrant County area. Foster for the rescue so I have experience with ferrets with medical needs.
-          </p>
-        </div>
-      </div>
-      <div class="w-1/2 mx-auto mt-6 text-lg">
-        For further ferret sitting options, see the Facebook page, Ferret Sitters Connections.
-        <span>
-          <a
-            href="https://www.facebook.com/groups/344263992897139"
-            class="hover:underline text-blue-400 hover:text-blue-500"
-          >
-            https://www.facebook.com/groups/344263992897139
-          </a>
-        </span>
       </div>
     </div>
     """
