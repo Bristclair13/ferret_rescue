2247ea3058b9a9c502ce09b4f7fbcf0e60d7883c
Brianna St Clair <briannastclair13@gmail.com>
2024-10-23 21:46:55 -0600

    updated form

--- lib/ferret_rescue_web/live/adopt.ex
+++ lib/ferret_rescue_web/live/adopt.ex
@@ -1,10 +1,11 @@
 defmodule FerretRescueWeb.Live.Adopt do
   use FerretRescueWeb, :live_view
 
-  def mount(_params, session, socket) do
-    user = session["current_user"]
-    form = to_form(%{"ferret_questions" => ""})
-    {:ok, assign(socket, user: user, form: form)}
+  alias FerretRescue.Schemas.Application
+
+  def mount(_params, _session, socket) do
+    changeset = Application.changeset(%{})
+    {:ok, assign(socket, changeset: changeset)}
   end
 
   def render(assigns) do
@@ -31,21 +32,29 @@ defmodule FerretRescueWeb.Live.Adopt do
         If you are less than 18, a parent must fill out this form and be the responsible party.
       </p>
       <h3 class="text-3xl">Personal Information</h3>
-      <.simple_form for={@form} phx-submit="ferret_questions">
-        <.input field={@form[:name]} label="Full Name" />
-        <.input field={@form[:age]} label="Age" />
-        <.input field={@form[:street]} label="Home Address" />
-        <.input field={@form[:city]} label="City" />
-        <.input field={@form[:state]} label="State" />
-        <.input field={@form[:zip_code]} label="Zip Code" />
-        <.input field={@form[:time_at_address]} label="How long at this address?" />
-        <.input field={@form[:phone_primary]} label="Primary Phone" />
-        <.input field={@form[:phone_secondary]} label="Secondary Phone (optional)" />
-        <.input field={@form[:email]} label="Email" />
+      <.form :let={f} for={@changeset} phx-submit="submit_application">
+        <.input field={f[:name]} label="Full Name" />
+        <.input field={f[:age]} label="Age" />
+        <.input field={f[:street]} label="Home Address" />
+        <.input field={f[:city]} label="City" />
+        <.input field={f[:state]} label="State" />
+        <.input field={f[:zip_code]} label="Zip Code" />
+        <.input field={f[:time_at_address]} label="How long at this address?" />
+        <.input field={f[:phone_primary]} label="Primary Phone" />
+        <.input field={f[:phone_secondary]} label="Secondary Phone (optional)" />
+        <.input field={f[:email]} label="Email" />
         <.input
-          field={@form[:people_at_address]}
+          field={f[:people_at_address]}
           type="textarea"
           label="How many people live at this house (or visit frequently such as grandchildren/stepchildren) and what are their ages?"
+        />
+        <.input
+          field={f[:home_type]}
+          type="select"
+          label="What type of home is this?"
+          options={[
+            {"House", "house"}
+          ]}
         />
         <div class="flex flex-col">
           <label for="home-type" class="mb-4">What type of home is this?</label>
@@ -95,7 +104,7 @@ defmodule FerretRescueWeb.Live.Adopt do
           </select>
         </div>
         <.input
-          field={@form[:other_animals]}
+          field={f[:other_animals]}
           label="What animals/pets do you currently own?"
           type="textarea"
         />
@@ -111,7 +120,7 @@ defmodule FerretRescueWeb.Live.Adopt do
           </select>
         </div>
         <.input
-          field={@form[:vet_info]}
+          field={f[:vet_info]}
           label="Please provide your veterinarian's name and address?"
           type="textarea"
         />
@@ -128,39 +137,39 @@ defmodule FerretRescueWeb.Live.Adopt do
         </div>
         <h3 class="text-2xl">How much do you know about ferrets?</h3>
         <.input
-          field={@form[:eat_info]}
+          field={f[:eat_info]}
           label="What should a ferret eat and drink? How often? What treats are OK and not OK?"
           type="textarea"
         />
 
         <.input
-          field={@form[:kept_info]}
+          field={f[:kept_info]}
           label="Where should a ferret be kept and why? In the house? Garage? Backyard?"
           type="textarea"
         />
         <.input
-          field={@form[:proofing_info]}
+          field={f[:proofing_info]}
           label="What is ferret proofing and how do you do it?"
           type="textarea"
         />
         <.input
-          field={@form[:num_ferrets_info]}
+          field={f[:num_ferrets_info]}
           label="What is a good number of ferrets to have and why?"
           type="textarea"
         />
-        <.input field={@form[:cage_info]} label="When should a ferret be in a cage?" type="textarea" />
+        <.input field={f[:cage_info]} label="When should a ferret be in a cage?" type="textarea" />
         <.input
-          field={@form[:play_info]}
+          field={f[:play_info]}
           label="How often should a ferret be allowed out of his cage? Where should he play and with whom?"
           type="textarea"
         />
         <.input
-          field={@form[:toy_info]}
+          field={f[:toy_info]}
           label="What should a ferret be allowed to play with? Give examples of right toys and wrong toys?"
           type="textarea"
         />
         <.input
-          field={@form[:diseases_info]}
+          field={f[:diseases_info]}
           label="What diseases do ferrets get and what is the treatment?"
           type="textarea"
         />
@@ -187,22 +196,18 @@ defmodule FerretRescueWeb.Live.Adopt do
           </select>
         </div>
         <.input
-          field={@form[:heartworm_prevent]}
+          field={f[:heartworm_prevent]}
           label="How do you prevent heartworms in ferrets?"
           type="textarea"
         />
         <.input
-          field={@form[:move_info]}
+          field={f[:move_info]}
           label="Under what conditions would you move to a place which would not accept ferrets? What would you do with yours?"
           type="textarea"
         />
+        <.input field={f[:forever_home]} label="What does FOREVER HOME mean to you?" type="textarea" />
         <.input
-          field={@form[:forever_home]}
-          label="What does FOREVER HOME mean to you?"
-          type="textarea"
-        />
-        <.input
-          field={@form[:notes]}
+          field={f[:notes]}
           label="Notes, aynthing else you think we need to know, or questions you may have for us:"
           type="textarea"
         />
@@ -223,8 +228,14 @@ defmodule FerretRescueWeb.Live.Adopt do
             By selecting this box and clicking the submit button below I am confirming that I understand ferrets are not caged animals, they are not like hamsters and mice, that they must have time out of a cage daily for their well being both physically and mentally and that they do need human interaction with their play. I am ready to commit to giving the proper time and care to the ferret and I realize they depend completely on my schedule to determine when they will play, sleep and eat. Furthermore, I submit that my answers to the questions above are truthful and accurate to the best of my ability.
           </legend>
         </div>
-      </.simple_form>
+        <.button>Submit</.button>
+      </.form>
     </div>
     """
+  end
+
+  def handle_event("submit_application", %{"application" => application}, socket) do
+    changeset = Application.changeset(application) |> Map.put(:action, :insert)
+    {:noreply, assign(socket, changeset: changeset)}
   end
 end
