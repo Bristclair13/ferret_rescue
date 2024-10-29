a34e9e528d2a4923e3e1ee8b6cec4c40542b9d07
Bristclair13 <145925680+Bristclair13@users.noreply.github.com>
2024-10-28 19:20:00 -0600

    adding list sitters

---
+++ lib/ferret_rescue/resources/sitters/storage.ex
@@ -0,0 +1,11 @@
+defmodule FerretRescue.Resources.Sitters.Storage do
+  import Ecto.Query
+
+  alias FerretRescue.Schemas.Sitter
+  alias FerretRescue.Repo
+
+  @callback list_sitters(String.t()) :: [Sitter.t()]
+  def list_sitters() do
+    from(Sitter, order_by: :name) |> Repo.all()
+  end
+end
